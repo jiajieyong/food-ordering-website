dc094e3ce8fbb961ff3911baf61b315ea57f67ae
JiaJiey <jiajieyong91@gmail.com>
2024-04-16 19:38:28 +0800

    feat: working with form

--- src/components/composite/orderRow.tsx
+++ src/components/composite/orderRow.tsx
@@ -11,17 +11,28 @@ import { useAppDispatch } from '@/hooks/hooks';
 import { incrementQuantity, decrementQuantity, removeFromOrder } from '@/redux/orderSlice';
 import { Button } from "../ui/button";
 import { Trash2 } from  "lucide-react";
+import { UseFormRegister, UseFormSetValue, UseFormUnregister } from 'react-hook-form';
+import { IFormValues } from "./orderTable";
+
 interface IDetails {
     detail: IMenuItem;
+    index: number;
     quantity: number;
+    register: UseFormRegister<IFormValues>;
+    setValue: UseFormSetValue<IFormValues>;
+    unregister: UseFormUnregister<IFormValues>;
 }
 
 export function OrderRow(props: IDetails) {
     const dispatch = useAppDispatch();
-    const { detail, quantity } = props;
+    const { detail, index, quantity, register, setValue, unregister } = props;
     const { id, name, pricing } = detail;
     return (
         <TableRow>
+            <input type="hidden" {...register(`order.${index}.name`)} value={name} />
+            <input type="hidden" {...register(`order.${index}.pricing`)} value={pricing} />
+            <input type="hidden" {...register(`order.${index}.quantity`)} value={quantity} />
+
             <TableCell className="font-semibold">
                 {name}
             </TableCell>
@@ -33,15 +44,25 @@ export function OrderRow(props: IDetails) {
             </TableCell>
             <TableCell>
                 <Label htmlFor="price-1" className="sr-only">
-                Quantity
+                    Quantity
                 </Label>
                 <ToggleGroup
                     type="multiple"
                     variant="outline"
                 >
-                <ToggleGroupItem value="-" onClick={() => dispatch(decrementQuantity(id))}>-</ToggleGroupItem>
+                <ToggleGroupItem
+                    value="-"
+                    onClick={() => {
+                        dispatch(decrementQuantity(id));
+                        setValue(`order.${index}.quantity`, quantity-1);
+                    }}
+                >-
+                </ToggleGroupItem>
                 <div>{quantity}</div>
-                <ToggleGroupItem value="+" onClick={() => dispatch(incrementQuantity(id))}>+</ToggleGroupItem>
+                <ToggleGroupItem value="+" onClick={() => {
+                    dispatch(incrementQuantity(id))
+                    setValue(`order.${index}.quantity`, quantity+1);
+                }}>+</ToggleGroupItem>
                 </ToggleGroup>
             </TableCell>
             <TableCell>
@@ -52,7 +73,11 @@ export function OrderRow(props: IDetails) {
                     variant="outline"
                     size="sm"
                     className="flex items-center h-7 gap-1 text-sm"
-                    onClick={() => dispatch(removeFromOrder(id))}
+                    onClick={() => {
+                        dispatch(removeFromOrder(id));
+                        unregister(`order.${index}` );
+                        }
+                    }
                 >
                     <Trash2 className="h-3.5 w-3.5" />
                 </Button>
--- src/components/composite/orderTable.tsx
+++ src/components/composite/orderTable.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useForm } from 'react-hook-form';
 import {
     Table,
     TableBody,
@@ -12,41 +13,60 @@ import { OrderRow } from "./orderRow";
 import { useAppSelector } from '@/hooks/hooks';
 import { getTotalPrice } from '@/redux/orderSlice';
 
-export function OrderTable() {
+type IOrder = {
+    name: string,
+    quantity: number,
+    pricing: number
+}
 
+export interface IFormValues {
+    order: IOrder[]
+}
+
+export function OrderTable() {
     const menuItems = useAppSelector((state) => state.menuItem.menuItems);
     const orderItems = useAppSelector((state) => state.order.items);
     const totalPrice = useAppSelector(getTotalPrice);
+    const { register, handleSubmit, setValue, unregister } = useForm<IFormValues>();
+
+    const onSubmit = (data:any) => console.log(data);
 
     return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead className="w-[100px]">Item</TableHead>
-                    <TableHead>Price</TableHead>
-                    <TableHead>Quantity</TableHead>
-                    <TableHead className="w-[100px]">Total</TableHead>
-                    <TableHead />
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {Object.entries(orderItems).map(([id, quantity], index) => (
-                    <OrderRow
-                        key={index}
-                        detail={menuItems[id]}
-                        quantity={quantity}
-                    />
-                ))}
-            </TableBody>
-            <TableFooter>
-                <TableRow>
-                    <TableCell>Total</TableCell>
-                    <TableCell/>
-                    <TableCell/>
-                    <TableCell>S$ {totalPrice}</TableCell>
-                    <TableCell/>
-                </TableRow>
-            </TableFooter>
-        </Table>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Item</TableHead>
+                        <TableHead>Price</TableHead>
+                        <TableHead>Quantity</TableHead>
+                        <TableHead className="w-[100px]">Total</TableHead>
+                        <TableHead />
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {Object.entries(orderItems).map(([id, quantity], index) => (
+                        <OrderRow
+                            key={index}
+                            index={index}
+                            detail={menuItems[id]}
+                            quantity={quantity}
+                            register={register}
+                            setValue={setValue}
+                            unregister={unregister}
+                        />
+                    ))}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell>Total</TableCell>
+                        <TableCell/>
+                        <TableCell/>
+                        <TableCell>S$ {totalPrice}</TableCell>
+                        <TableCell/>
+                    </TableRow>
+                </TableFooter>
+            </Table>
+            <input type="submit" />
+        </form>
     )
 }
