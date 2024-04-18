d4a5853571f452b3618e07260efa673ceb21aa49
JiaJiey <jiajieyong91@gmail.com>
2024-04-19 00:01:12 +0800

    feat: update order number on the nav bar

--- src/components/composite/orderNavigation.tsx
+++ src/components/composite/orderNavigation.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link';
 import { useAppSelector } from '@/hooks/hooks';
 import { ReceiptText } from 'lucide-react';
-import { getMemoizedNumItems } from '@/redux/orderSlice';
 
 const OrderNavigation = () => {
-    const getCartItems = useAppSelector(getMemoizedNumItems);
+    const order = useAppSelector((state) => state.order.queueNumber);
     return (
         <span className="flex space-x-2 items-center">
             <Link href="/queue" className="flex space-x-2 justify-between items-center font-bold text-orange-500">
                 <ReceiptText />
-                <div>{getCartItems}</div>
+                <div>{order.length}</div>
             </Link>
         </span>
     );
--- src/components/composite/queue.tsx
+++ src/components/composite/queue.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
 import { getQueue } from "@/redux/queueSlice";
+import { RefreshCcw } from 'lucide-react';
 
 import {
     Card,
@@ -33,16 +34,22 @@ export function Queue() {
                   <main className="grid flex-1 items-start gap-4 p-4 mobile:px-6 mobile:py-0 tablet:gap-8 laptop:grid-cols-3 desktop:grid-cols-3">
                   <div className="grid auto-rows-max items-start gap-4 tablet:gap-8 laptop:col-span-3">
                     <div className="grid gap-4 mobile:grid-cols-2 tablet:grid-cols-4 laptop:grid-cols-2 desktop:grid-cols-4">
-                      { order.length > 0 &&
                         <Card className="mobile:col-span-2">
                           <CardHeader className="py-3">
-                            <CardTitle>Your Orders</CardTitle>
+                            <CardTitle className='flex item-center gap-4'>
+                              Your Orders
+                                <RefreshCcw
+                                  onClick={
+                                    () => dispatch(getQueue())
+                                    }
+                                  />
+                              </CardTitle>
                             <CardDescription className="max-w-lg text-balance leading-relaxed">
                               <div className="text-4xl">{order.join(', ')}</div>
+                              {order.length === 0 && <div className="text-s text-muted-foreground">No pending orders</div>}
                             </CardDescription>
                           </CardHeader>
                         </Card>
-                      }
                       <Card>
                         <CardHeader className="py-3">
                           <CardDescription>Ready for collection</CardDescription>
