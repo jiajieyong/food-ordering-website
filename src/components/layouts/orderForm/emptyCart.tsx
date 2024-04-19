472d0b78243e3ded8899621d6e3d327960fe5e82
JiaJiey <jiajieyong91@gmail.com>
2024-04-19 17:17:02 +0800

    feat: dropdown selection with menu items populated

--- src/components/layouts/orderForm/emptyCart.tsx
+++ src/components/layouts/orderForm/emptyCart.tsx
@@ -1,10 +1,25 @@
+import Image from "next/image";
+import _ from 'lodash';
 import {
     TableRow,
     TableCell
 } from "@/components/ui/table";
-import Image from "next/image";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
+import { useMenuItems } from "@/hooks/useMenuItems";
+
 
 export const EmptyCart = () => {
+    const menu = useMenuItems();
+    const menuGroup = Object.groupBy(Object.values(menu), ({ category}) => category);
+
     return (
         <TableRow>
             <TableCell colSpan={2}>
@@ -18,6 +33,27 @@ export const EmptyCart = () => {
             </TableCell>
             <TableCell colSpan={4}>
                 Oh no! Your cart is empty
+                <Select>
+                    <SelectTrigger className="w-[280px] my-4">
+                        <SelectValue placeholder="Select a menu item" />
+                    </SelectTrigger>
+                    <SelectContent className="bg-white">
+                        {
+                            Object.entries(menuGroup).map(([category, items], index) => {
+                                return (
+                                    <SelectGroup key={index}>
+                                        <SelectLabel>{_.startCase(category).toString()}</SelectLabel>
+                                        {
+                                            items && items.map((item: IMenuItem, index: number) => {
+                                                return <SelectItem key={index} value={item.id}>{item.name}</SelectItem>
+                                            })
+                                        }
+                                    </SelectGroup>
+                                )
+                            })
+                        }
+                    </SelectContent>
+                </Select>
             </TableCell>
         </TableRow>
     )
