2c9dd03f8c1918f19278d66f5efe2a3ce6cb8483
JiaJiey <jiajieyong91@gmail.com>
2024-04-21 00:10:09 +0800

    feat: eslint import sort order

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from "globals";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+
+export default config = [
+  {languageOptions: { globals: globals.browser },},
+  ...compat.extends("standard-with-typescript"),
+  pluginReactConfig,
+];
